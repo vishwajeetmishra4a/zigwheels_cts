f9feac9064cb08804cdef2f4335e8c5f16fe49cb
Mishra <2488303@cognizant.com>
2026-05-28 13:18:08 +0530

    Updates in ElectricBikes Class

--- Zigwheels/src/main/java/pages/ElectricBikes.java
+++ Zigwheels/src/main/java/pages/ElectricBikes.java
@@ -3,8 +3,6 @@ package pages;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.*;
 import org.openqa.selenium.interactions.Actions;
-//import org.openqa.selenium.support.pagefactory.*;
-//import org.openqa.selenium.support.FindBy;
 
 import java.util.*;
 import java.time.Duration;
@@ -24,19 +22,10 @@ public class ElectricBikes {
 		this.action=new Actions(driver);
 	}
 	
-//	@FindBy(xpath="//span[contains(text(),'MORE')]")
-//	WebElement moreBtn;
-//	
-//	@FindBy(xpath="//a[@data-track-label='nav-electric-vehicles']")
-//	WebElement eBike;
 	
 	public void gotoPage() {
 		WebElement moreBtn=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'MORE')]")));
-		action.moveToElement(moreBtn).perform();
-		
-//		wait.until(ExpectedConditions.elementToBeClickable(moreBtn));
-//		wait.until(ExpectedConditions.visibilityOf(moreBtn));
-//		action.moveToElement(moreBtn).perform();		
+		action.moveToElement(moreBtn).perform();	
 		
 		WebElement eBike=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@data-track-label='nav-electric-vehicles']")));
 		eBike.click();
